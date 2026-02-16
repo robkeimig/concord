51eaac38a1088424a3d4e640b0522cf347d345e1
Rob Keimig <rob@zenify.io>
2026-02-16 11:25:44 -0600

    Some cleanup

--- WinFormsClient/MainForm.cs
+++ WinFormsClient/MainForm.cs
@@ -1,128 +1,113 @@
 using Concord.WinForms;
 using Microsoft.Web.WebView2.Core;
 
-namespace WinFormsClient
+namespace WinFormsClient;
+
+public partial class MainForm : Form
 {
-    public partial class MainForm : Form
+    bool Initialized;
+    Configuration Configuration;
+    SettingsForm SettingsForm;
+    AddServerForm AddServerForm;
+
+    public MainForm()
     {
-        bool Initialized;
-        Configuration Configuration;
-        SettingsForm SettingsForm;
-        AddServerForm AddServerForm;
+        InitializeComponent();
+        LoadingPanel.BringToFront();
+        Configuration = Configuration.Load();
+        AddNewServerButton.Click += (_, _) => ShowAddServerDialog();
+        EnsureWebView();
+    }
 
-        public MainForm()
+    private sealed class StatusStripScope : IDisposable
+    {
+        private readonly StatusStrip _strip;
+        private readonly bool _wasEnabled;
+
+        public StatusStripScope(StatusStrip strip)
         {
-            InitializeComponent();
-            LoadingPanel.BringToFront();
-            Configuration = Configuration.Load();
-
-            AddNewServerButton.Click += (_, _) => ShowAddServerDialog();
-
-            EnsureWebView();
-            //ApplyDarkTheme(this);
+            _strip = strip;
+            _wasEnabled = strip.Enabled;
+            _strip.Enabled = false;
         }
 
-        private sealed class StatusStripScope : IDisposable
+        public void Dispose()
         {
-            private readonly StatusStrip _strip;
-            private readonly bool _wasEnabled;
+            _strip.Enabled = _wasEnabled;
+        }
+    }
+    
+    private void NavigateToCurrentServer()
+    {
+        if (Configuration.Servers.Count == 0)
+        {
+            MainWebView.Source = new Uri("https://app/NoServers.html");
+            return;
+        }
 
-            public StatusStripScope(StatusStrip strip)
+        var server = Configuration.LastServerId.HasValue
+            ? Configuration.Servers.First(x => x.Id == Configuration.LastServerId.Value)
+            : Configuration.Servers.First();
+
+        MainWebView.Source = new Uri($"https://{server.IpAddress}");
+    }
+
+    private async void EnsureWebView()
+    {
+        var environment = await CoreWebView2Environment.CreateAsync(
+           userDataFolder: Path.Combine(
+               Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+               "ConcordWebView"
+           )
+        );
+
+        SettingsForm = new SettingsForm(environment, Configuration);
+        AddServerForm = new AddServerForm(environment, Configuration);
+
+        await MainWebView.EnsureCoreWebView2Async(environment);
+        MainWebView.NavigationCompleted += HandleWebViewNavigationCompleted;
+
+        MainWebView.CoreWebView2.SetVirtualHostNameToFolderMapping(
+            hostName: "app",
+            folderPath: Path.Combine(AppContext.BaseDirectory, "wwwroot"),
+            accessKind: CoreWebView2HostResourceAccessKind.Allow
+        );
+
+        NavigateToCurrentServer();
+    }
+
+    private void HandleWebViewNavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
+    {
+        LoadingPanel.Visible = false;
+
+        if (!Initialized)
+        {
+            Initialized = true;
+        }
+    }
+
+    private void ShowAddServerDialog()
+    {
+        using (new StatusStripScope(MainStatusStrip))
+        {
+            AddServerForm.StartPosition = FormStartPosition.CenterParent;
+            var result = AddServerForm.ShowDialog(this);
+
+            if (result == DialogResult.OK)
             {
-                _strip = strip;
-                _wasEnabled = strip.Enabled;
-
-                // Disable so it can't be interacted with.
-                _strip.Enabled = false;
-            }
-
-            public void Dispose()
-            {
-                _strip.Enabled = _wasEnabled;
-            }
-        }
-
-        private StatusStripScope SuppressMainStatusStrip()
-        {
-            // `MainStatusStrip` is the designer-created instance.
-            return new StatusStripScope(MainStatusStrip);
-        }
-
-
-
-        private void NavigateToCurrentServer()
-        {
-            if (Configuration.Servers.Count == 0)
-            {
-                MainWebView.Source = new Uri("https://app/NoServers.html");
-                return;
-            }
-
-            var server = Configuration.LastServerId.HasValue
-                ? Configuration.Servers.First(x => x.Id == Configuration.LastServerId.Value)
-                : Configuration.Servers.First();
-
-            MainWebView.Source = new Uri($"https://{server.IpAddress}");
-        }
-
-        private async void EnsureWebView()
-        {
-            var environment = await CoreWebView2Environment.CreateAsync(
-               userDataFolder: Path.Combine(
-                   Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                   "ConcordWebView"
-               )
-            );
-
-            SettingsForm = new SettingsForm(environment, Configuration);
-            AddServerForm = new AddServerForm(environment, Configuration);
-
-            await MainWebView.EnsureCoreWebView2Async(environment);
-            MainWebView.NavigationCompleted += HandleWebViewNavigationCompleted;
-
-            MainWebView.CoreWebView2.SetVirtualHostNameToFolderMapping(
-                hostName: "app",
-                folderPath: Path.Combine(AppContext.BaseDirectory, "wwwroot"),
-                accessKind: CoreWebView2HostResourceAccessKind.Allow
-            );
-
-            NavigateToCurrentServer();
-        }
-
-        private void HandleWebViewNavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
-        {
-            // Hide loading UI once navigation completes.
-            LoadingPanel.Visible = false;
-
-            if (!Initialized)
-            {
-                Initialized = true;
+                
+                Configuration = Configuration.Load();
+                NavigateToCurrentServer();
             }
         }
+    }
 
-        private void ShowAddServerDialog()
+    private void ShowSettingsDialog(object sender, EventArgs e)
+    {
+        using (new StatusStripScope(MainStatusStrip))
         {
-            using (SuppressMainStatusStrip())
-            {
-                AddServerForm.StartPosition = FormStartPosition.CenterParent;
-                var result = AddServerForm.ShowDialog(this);
-
-                if (result == DialogResult.OK)
-                {
-                    
-                    Configuration = Configuration.Load();
-                    NavigateToCurrentServer();
-                }
-            }
-        }
-
-        private void ShowSettingsDialog(object sender, EventArgs e)
-        {
-            using (SuppressMainStatusStrip())
-            {
-                SettingsForm.StartPosition = FormStartPosition.CenterParent;
-                SettingsForm.ShowDialog(this);
-            }
+            SettingsForm.StartPosition = FormStartPosition.CenterParent;
+            SettingsForm.ShowDialog(this);
         }
     }
 }
