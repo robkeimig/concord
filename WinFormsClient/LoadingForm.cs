aae13f9a55533a2716102f092ed76a51168e78d8
Rob Keimig <rob@zenify.io>
2026-02-19 17:02:50 -0600

    Disable interactions with the loading video loop

--- WinFormsClient/LoadingForm.cs
+++ WinFormsClient/LoadingForm.cs
@@ -9,15 +9,18 @@ public partial class LoadingForm : Form
         FormBorderStyle = FormBorderStyle.None;
         StartPosition = FormStartPosition.CenterScreen;
         TopMost = true;
-        this.Width = 576;
-        this.Height = 700;
-        this.Left = (Screen.PrimaryScreen.Bounds.Width - this.Width) / 2;
-        this.Top = (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2;
-        
+        Width = 576;
+        Height = 700;
+        Left = (Screen.PrimaryScreen.Bounds.Width - this.Width) / 2;
+        Top = (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2;
         string videoPath = Path.Combine(Application.StartupPath, "Assets", "LoadingLoop.mp4");
+        LoadingLoopPlayer.enableContextMenu = false;
+        LoadingLoopPlayer.fullScreen = false;
         LoadingLoopPlayer.URL = videoPath;
         LoadingLoopPlayer.settings.setMode("loop", true);
         LoadingLoopPlayer.uiMode = "none";
         LoadingLoopPlayer.Ctlcontrols.play();
+        LoadingLoopPlayer.Enabled = false;
+
     }
 }
